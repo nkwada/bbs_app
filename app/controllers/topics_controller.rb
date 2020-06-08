df598dd784618fb602beb933d09d8b28c77964b1
nkwada <n.qb.dft9@gmail.com>
2020-06-08 10:52:55 +0000

    [Add]Postsコントローラとモデルの追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,25 @@
 class TopicsController < ApplicationController
 	def top
 	end
+
+	def index
+		@topics = Topic.all
+		@newTopic = Topic.new
+	end
+
+	def create
+	  @topic = Topic.new(params[:topic].permit(:title))
+	  @topic.save
+	  redirect_to index_path
+	end
+
+	def show
+	  @topic = Topic.find(params[:id])
+	end
+
+	def delete
+	  @topic = Topic.find(params[:id])
+	  @topic.destroy
+	  redirect_to index_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'topics#top'
+  get 'index' => 'topics#index'
+  get 'show/:id' => 'topics#show', as: :topics_show
+  post 'topics/create' => 'topics#create'
+  delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
 end
